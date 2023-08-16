122884c249e32fe423dffa4f60a7308962ce585e
SAM-PRAKASH <ssamprakash96@gmail.com>
2023-08-16 11:59:35 +0530

    completed

--- React-task/price-tag/src/App.js
+++ React-task/price-tag/src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
+const featuresOfCard1 = ["✔ Single User", "✔ 50GB Storage", "✔ Unlimited Public Projects", "✔ Community Access", "✖ Unlimited Private Projects", "✖ Dedicated Phone Support", "✖ Free Subdomain", "✖ Monthly Status Reports"]
+const featuresOfCard2 = ["✔ 5 User", "✔ 50GB Storage", "✔ Unlimited Public Projects", "✔ Community Access", "✔ Unlimited Private Projects", "✔ Dedicated Phone Support", "✔ Free Subdomain", "✖ Monthly Status Reports"]
+const featuresOfCard3 = ["✔ Unlimited User", "✔ 50GB Storage", "✔ Unlimited Public Projects", "✔ Community Access", "✔ Unlimited Private Projects", "✔ Dedicated Phone Support", "✔ Free Subdomain", "✔ Monthly Status Reports"]
 function App() {
   return (
-    <div class="container">
-        <div class="Plans" id="P1">
-            <h4>FREE</h4>
-            <h1>$0/month</h1>
-            <button class="button button5">Button</button>
+    <div className="App">
+      <div class="CardContainer">
+        <div class="PriceCards">
+          <PriceCards
+            cardType="FREE"
+            validity="$0/month"
+            features={featuresOfCard1}
+          />
         </div>
-        <div class="Plans" id="P2">
-            <button class="button button5">Button</button>
+        <div class="PriceCards">
+          <PriceCards
+            cardType="PLUS"
+            validity="$9/month"
+            features={featuresOfCard2}
+          />
         </div>
-        <div class="Plans" id="p3">
-            <button class="button button5">Button</button>
+        <div class="PriceCards">
+          <PriceCards
+            cardType="PRO"
+            validity="$49/month"
+            features={featuresOfCard3}
+          />
         </div>
+      </div>
     </div>
   );
 }
 
+function PriceCards({ cardType, validity, features }) {
+  return (
+    <>
+      <h4>{cardType}</h4>
+      <h1>{validity}</h1>
+      <div className="line"></div>
+      {features && (
+        <ul>
+          {features.map((feature, index) => (
+            <p className={`features ${feature.includes('✖') ? 'red' : feature.includes('✔') ? 'green' : ''}`} key={index}>
+              {feature}
+            </p>
+          ))}
+        </ul>
+      )}
+      <button class="button button5">Button</button>
+    </>
+  )
+}
 export default App;
